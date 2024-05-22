93173d8b755e8b20b7b901ab902dafd55dc805a1
Cornelius-Figgle <max@fullimage.net>
2024-05-22 17:58:50 +0100

    randomart maybe working?

--- figglefetch.lua
+++ figglefetch.lua
@@ -48,10 +48,86 @@ function osname()
   return name_pp
 end
 
+-- generates ascii randomart from the OS name, hostname and username
+function generateart(os, host, user, algorithm)
+  -- set image sizes
+  local dimensions = {8, 5}
+  local len = dimensions[1] * dimensions[2]
+
+  -- trim/repeat os string to 20 characters
+  if #os > len then
+    os = os:sub(1, len)
+  elseif #os < len then
+    os = string.rep(os, len // #os)
+    os = os .. os:sub(1, len - #os)
+  end
+  
+  -- trim/repeat hostname string to len characters
+  if #host > len then
+    host = host:sub(1, len)
+  elseif #host < len then
+    host = string.rep(host, len // #host)
+    host = host .. host:sub(1, len - #host)
+  end
+  
+  -- trim/repeat username string to len characters
+  if #user > len then
+    user = user:sub(1, len)
+  elseif #user < len then
+    user = string.rep(user, len // #user)
+    user = user .. user:sub(1, len - #user)
+  end
+
+  -- joins the strings together in a 2:1:1 ratio, then finds the decimal of each character  
+  local codes = {}
+  local ratio = {
+    (len // 2) + (len - (len // 2 + len // 4 + len // 4)),
+    len // 4,
+    len // 4
+  }
+  local string = os:sub(1, ratio[1]) .. host:sub(1, ratio[2]) .. user:sub(1, ratio[3])
+  for char in string:gmatch(".") do
+    table.insert(codes, char:byte())
+  end
+
+  -- converts codes to characters
+  local art_chars = {}
+  for i,v in pairs(codes) do
+    if v == 32 or v == 45 then
+      art_chars[i] = tostring(46):char()
+    elseif v >= 65 and v <= 90 then
+      art_chars[i] = tostring(v-33):char()
+    elseif v >= 97 and v <= 102 then
+      art_chars[i] = tostring(v-39):char()
+    elseif v >= 103 and v <= 108 then
+      art_chars[i] = tostring(v-12):char()
+    elseif v >= 109 and v <= 112 then
+      art_chars[i] = tostring(v+14):char()
+    elseif v >= 113 and v <= 118 then
+      art_chars[i] = tostring(v-22):char()
+    elseif v >= 119 and v <= 122 then
+      art_chars[i] = tostring(v+4):char()
+    end
+  end
+
+  -- joins the table of ascii codes into a rectangle
+  local art = ""
+  for i=1,dimensions[2] do
+    art = art .. table.concat(art_chars, "", dimensions[1]*(i-1)+1, dimensions[1]*i) .. "\n"
+  end
+
+  return art
+end
+
+-- combines the randomart and the data and prints it
+function printoutput(output, art)
+  --
+end
+
 
 -- define table for sysinfo
 data = {}
-data.line = string.rep("-", 10)
+data.line = string.rep("-", 20)
 
 -- gather data
 data.user = runcommand("whoami")
@@ -68,6 +144,8 @@ output = string.format(
   data.user, data.host, data.uptime, data.os
 )
 
+art = generateart(data.os, data.host, data.user)
+print(art)
 
 -- final display & exit
 print(output)
